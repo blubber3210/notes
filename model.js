2c286653ecf02ad9fc4cfc3a96e32c221a0de9df
Elida <blubber3210@gmail.com>
2025-11-20 11:21:01 +0100

    fix?

--- model.js
+++ model.js
@@ -1,16 +1,4 @@
 
-let content = '';
-
-// Operator examples
-
-let num1 = 0;
-let num2 = 0;
-let selectedOperator = '';
-let operatorResult = 0;
-
-// string examples
-let stringExample = `This is a string example, with some text. I don't know what it should say.`;
-
 const model = {
     app: {
         currentPage: 'homePage',
@@ -31,3 +19,18 @@ const model = {
         text: '',
     }
 }
+
+
+
+let content = '';
+
+// Operator examples
+
+let num1 = 0;
+let num2 = 0;
+let selectedOperator = '';
+let operatorResult = 0;
+
+// string examples
+let stringExample = `This is a string example, with some text. I don't know what it should say.`;
+
